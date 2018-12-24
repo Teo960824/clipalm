2255a5196c33d409d9c9f64ecd5470b7a09cbfd9
duanzhichao <duanzhichao2008@163.com>
2018-12-24 10:27:14 +0800

    修复am-list报错的bug

--- src/utils/config.js
+++ src/utils/config.js
@@ -7,20 +7,20 @@
 //   router: 'api'
 // }
 
-// // 开发配置
-// module.exports = {
-//   http: 'http://192.168.0.79',
-//   static: 'http://www.jiankanglaifu.com',
-//   drgserver: 'http://192.168.0.79',
-//   port: '3009',
-//   router: 'api'
-// }
-
-// 服务器配置
+// 开发配置
 module.exports = {
-  http: 'http://www.jiankanglaifu.com',
+  http: 'http://192.168.0.79',
   static: 'http://www.jiankanglaifu.com',
-  drgserver: 'http://139.129.165.56',
-  port: '80',
-  router: 'drgwork/api'
+  drgserver: 'http://192.168.0.79',
+  port: '3009',
+  router: 'api'
 }
+
+// // 服务器配置
+// module.exports = {
+//   http: 'http://www.jiankanglaifu.com',
+//   static: 'http://www.jiankanglaifu.com',
+//   drgserver: 'http://139.129.165.56',
+//   port: '80',
+//   router: 'drgwork/api'
+// }
