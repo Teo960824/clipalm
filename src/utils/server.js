927050ca8900b2edba7d8f64fad6310ccc1cbc5d
duanzhichao <duanzhichao2008@163.com>
2019-01-03 15:49:17 +0800

    修复查询bug

--- src/utils/server.js
+++ src/utils/server.js
@@ -14,10 +14,7 @@ export function getServer (obj, activeTab, menu, value = null) {
   if (activeTab === 1) {
     timeout = 30000
   }
-  // modal.toast({ message: url, duration: 1 })
-  // console.log(url)
   storage.getItem(url, e => {
-    console.log(e.result)
     if (e.result === 'success!') {
       const edata = JSON.parse(e.data)
       setStore(obj, menu, 'type', edata)
@@ -31,7 +28,6 @@ export function getServer (obj, activeTab, menu, value = null) {
         responseType: 'json',
         url: url
       }, function (res) {
-        console.log(res.ok)
         if (res.ok) {
           storage.setItem(url, JSON.stringify(res.data))
           setStore(obj, activeTab, menu, res.data)
@@ -67,25 +63,7 @@ function setStore (obj, activeTab, menu, rdata) {
   let details = {}
   const infoLevel = obj.$store.state.Home.infoLevel
   switch (activeTab) {
-    case 1:
-      if (['未入组病历列表', 'QY病历列表', '低风险死亡病历列表', '费用异常病历列表'].includes(menu)) {
-        data = obj.$store.state.Edit.wt4PopRight
-        data = data.concat(rdata.data[0].wt4)
-        obj.$store.commit('SET_wt4PopRight', data)
-        wt4Data = rdata.data[0]
-        wt4Data.wt4 = data
-        obj.$store.commit('SET_infoLevel', infoLevel + 1)
-        details = getDetails(obj, menu, wt4Data)
-        obj.$store.commit('SET_info', details)
-      } else {
-        obj.$store.commit('SET_wt4PopRight', [])
-        data = obj.$store.state.Edit.wt4Case
-        data = data.concat(rdata.data)
-        obj.$store.commit('SET_wt4Info', rdata.info)
-        obj.$store.commit('SET_wt4Case', data)
-      }
-      break
-    case 2:
+    case 0:
       if (['ADRG规则', 'DRG规则', 'ICD10-亚目', 'ICD10-细目', 'ICD9-亚目', 'ICD9-细目'].includes(menu)) {
         if (['ICD10-亚目', 'ICD9-亚目'].includes(menu)) {
           data.icd = rdata.data.map((x) => {
@@ -106,7 +84,25 @@ function setStore (obj, activeTab, menu, rdata) {
         obj.$store.commit('SET_rule', data)
       }
       break
-    case 3:
+    case 1:
+      if (['未入组病历列表', 'QY病历列表', '低风险死亡病历列表', '费用异常病历列表'].includes(menu)) {
+        data = obj.$store.state.Edit.wt4PopRight
+        data = data.concat(rdata.data[0].wt4)
+        obj.$store.commit('SET_wt4PopRight', data)
+        wt4Data = rdata.data[0]
+        wt4Data.wt4 = data
+        obj.$store.commit('SET_infoLevel', infoLevel + 1)
+        details = getDetails(obj, menu, wt4Data)
+        obj.$store.commit('SET_info', details)
+      } else {
+        obj.$store.commit('SET_wt4PopRight', [])
+        data = obj.$store.state.Edit.wt4Case
+        data = data.concat(rdata.data)
+        obj.$store.commit('SET_wt4Info', rdata.info)
+        obj.$store.commit('SET_wt4Case', data)
+      }
+      break
+    case 2:
       if (['ADRG分析', 'DRG分析', '诊断基础-亚目', '诊断DRG分析', '手术基础-亚目', '手术DRG分析'].includes(menu)) {
         obj.$store.commit('SET_infoLevel', infoLevel + 1)
         details = getDetails(obj, menu, rdata.data[0])
@@ -133,7 +129,7 @@ function setStore (obj, activeTab, menu, rdata) {
         obj.$store.commit('SET_statDrg', data)
       }
       break
-    case 4:
+    case 3:
       switch (menu) {
         case '帖子列表':
           obj.$store.commit('SET_post', rdata.data)
@@ -189,15 +185,15 @@ export function customSearch (obj, value) {
   }
   let data = []
   switch (obj.$store.state.Home.activeTab) {
+    case 0:
+      value.tab = '字典'
+      value.page = obj.$store.state.Library.page
+      break
     case 1:
       value.tab = '病案'
       value.page = obj.$store.state.Edit.wt4Page
       break
     case 2:
-      value.tab = '字典'
-      value.page = obj.$store.state.Library.page
-      break
-    case 3:
       value.tab = 'DRG分析'
       value.page = obj.$store.state.Stat.statPage
       break
@@ -216,17 +212,17 @@ export function customSearch (obj, value) {
     if (res.ok) {
       obj.$store.commit('SET_menu', [obj.$store.state.Home.activeTab, '自定义查询结果'])
       switch (obj.$store.state.Home.activeTab) {
+        case 0:
+          data = obj.$store.state.Library.rule
+          data = data.concat(res.data.data)
+          obj.$store.commit('SET_rule', data)
+          break
         case 1:
           data = obj.$store.state.Edit.wt4Case
           data = data.concat(res.data.data)
           obj.$store.commit('SET_wt4Case', data)
           break
         case 2:
-          data = obj.$store.state.Library.rule
-          data = data.concat(res.data.data)
-          obj.$store.commit('SET_rule', data)
-          break
-        case 3:
           res.data.data = res.data.data.map((x) => {
             x.name1 = x.name
             return x
