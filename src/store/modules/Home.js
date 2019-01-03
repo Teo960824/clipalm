0f988c7a9130cd35e49391b84c9f8514e3df5aaf
duanzhichao <duanzhichao2008@163.com>
2019-01-03 15:40:00 +0800

    合并master

--- src/store/modules/Home.js
+++ src/store/modules/Home.js
@@ -11,8 +11,8 @@ const state = {
   userAnalyse: null,
   isLoadingShow: false,
   // 发布版本修改
-  version: '1.0.4',
-  serverVersion: { version: '1.0.4' },
+  version: '1.0.5',
+  serverVersion: { version: '1.0.5' },
   customQuery: [{show: false, query: {}}, {show: false, query: {}}, {show: false, query: {}}],
   forumInfo: {}
 }
