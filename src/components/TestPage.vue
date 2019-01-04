b7cc1e0b8f62123f600d8a4869044744d7eebc74
duanzhichao <duanzhichao2008@163.com>
2019-01-04 11:16:52 +0800

    增加loading组件

--- src/components/TestPage.vue
+++ src/components/TestPage.vue
@@ -1,42 +1,61 @@
 <template>
-  <div class="demo-container">
-    <div class="btn" @click="openLightBox">
-      <text class="btn-txt">点击按钮弹出全屏图片</text>
-    </div>
-    <wxc-lightbox
-      ref="wxc-lightbox"
-      height="800"
-      :show="show"
-      :image-list="imageList"
-      @wxcLightboxOverlayClicked="wxcLightboxOverlayClicked">
-    </wxc-lightbox>
+  <div class="mzui-demo">
+    <scroller class="scroller">
+      <!-- <title></title> -->
+
+      <div class="btn-wrap">
+        <!-- <fm-button size="huge" @buttonClicked="openCanClose" color="#dc2a2a">打开可关闭 loading</fm-button>
+        <fm-button size="huge" @buttonClicked="openCanClose" color="#04c0cf">最少加载 0.5s</fm-button>
+        <fm-button size="huge" @buttonClicked="openCantClose">打开不可关闭 waiting(慎点)</fm-button> -->
+        <wxc-button text="确定"
+              @wxcButtonClicked="openCantClose"></wxc-button>
+      </div>
+       <Waiting
+          ref="fm-waiting"
+          title="正在查询"
+          :can-auto-close="canAutoClose"></Waiting>
+    </scroller>
   </div>
 </template>
 
+<style scoped>
+  .mzui-demo {
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+    background-color: #ffffff;
+  }
+
+  .btn-wrap {
+    margin-top: 300px;
+    height: 450px;
+    justify-content: space-between;
+    align-items: center;
+  }
+</style>
+
 <script>
-  import { WxcLightbox } from 'weex-ui';
-  export default {
-    components: {
-      WxcLightbox
+import Waiting from './common/packages/loading'
+import { WxcButton } from 'weex-ui'
+export default {
+  components: { Waiting, WxcButton },
+  data: () => ({
+    canAutoClose: true
+  }),
+  methods: {
+    openCanClose () {
+      this.canAutoClose = true
+      this.$refs['fm-waiting'].active()
     },
-    data: function () {
-      return {
-        imageList: [
-          { src: 'https://gd2.alicdn.com/bao/uploaded/i2/T14H1LFwBcXXXXXXXX_!!0-item_pic.jpg' },
-          { src: 'https://gd1.alicdn.com/bao/uploaded/i1/TB1PXJCJFXXXXciXFXXXXXXXXXX_!!0-item_pic.jpg' },
-          { src: 'https://gd3.alicdn.com/bao/uploaded/i3/TB1x6hYLXXXXXazXVXXXXXXXXXX_!!0-item_pic.jpg' }
-        ],
-        show: false
-      };
-    },
-    methods: {
-      openLightBox () {
-        this.show = true;
-      },
-      wxcLightboxOverlayClicked () {
-      // 无状态组件，需要在此次关闭
-        this.show = false;
-      }
+    openCantClose () {
+      this.canAutoClose = false
+      this.$refs['fm-waiting'].active()
+      setTimeout(() => {
+        this.$refs['fm-waiting'].hide()
+      }, 6000);
     }
-  };
+  }
+};
 </script>
---
+++ src/components/common/packages/loading/index.vue
@@ -0,0 +1,325 @@
+<!-- CopyRight (C) 2018-2022 FlymeApps Team Holding Limited. -->
+<!-- Created by Yanjiie on 2018/4/26. -->
+
+<template>
+  <div>
+    <div v-if="show || showIn"
+         :style="wrapperStyle"
+         ref="fm-waiting"
+         class="fm-waiting"
+         @click="warpperClicked"
+         @touchend="handleTouchEnd">
+      <div :class="['waiting-wrapper', !isCreator ? 'waiting-wrapper-H5' : '']"
+           v-if="show || showIn">
+          <template v-if="!isCreator">
+            <div class="waiting-circle">
+                <div class="waiting--dot waiting--dot-1" ref="dot-1"></div>
+                <div class="waiting--dot waiting--dot-2" ref="dot-2"></div>
+                <div class="waiting--dot waiting--dot-3" ref="dot-3"></div>
+            </div>
+            <text class="waiting--text">{{ title }}</text>
+          </template>
+          <template v-else>
+            <FmNativeWaiting :text="title"></FmNativeWaiting>
+          </template>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Queue from './queue'
+const animation = weex.requireModule('animation')
+
+export default {
+  name: 'Waiting',
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    backgroundOpacity: {
+      type: Number,
+      default: 0.7
+    },
+    canAutoClose: {
+      type: Boolean,
+      default: true
+    }
+  },
+  data: () => ({
+    show: false,
+    showIn: false,
+    loadingStart: 0,
+    animating: false
+  }),
+  computed: {
+    wrapperStyle () {
+      const { showIn, backgroundOpacity } = this
+      return {
+        backgroundColor: `rgba(0, 0, 0, ${backgroundOpacity})`,
+        opacity: !showIn ? 0 : 1
+      }
+    },
+    isCreator () {
+      return weex.supports && weex.supports('@component/FmNativeWaiting')
+    }
+  },
+  methods: {
+    handleTouchEnd (e) {
+      // 原生上有点击穿透问题
+      e.preventDefault && e.preventDefault()
+    },
+    appear (bool) {
+      if (this.animating) {
+        return
+      }
+      const elm = this.$refs['fm-waiting']
+      if (!elm) {
+        this.showIn = bool
+        return
+      }
+      this.animating = true
+      animation.transition(elm, {
+        styles: {
+          opacity: bool ? 1 : 0
+        },
+        duration: 320,
+        timingFunction: 'cubic-bezier(0.33, 0, 0.66, 1)'
+      }, () => {
+        this.showIn = bool
+        if (bool) {
+          this.$emit('fmWaitingAppeared', false)
+        } else {
+          this.$emit('fmWaitingDisappeared', false)
+        }
+        this.animating = false
+      })
+    },
+    start () {
+      this.prepare()
+      this.dot1Queue.loop()
+      this.dot2Queue.loop()
+      this.dot3Queue.loop()
+    },
+    stop () {
+      this.dot1Queue && this.dot1Queue.stop()
+      this.dot2Queue && this.dot2Queue.stop()
+      this.dot3Queue && this.dot3Queue.stop()
+    },
+    warpperClicked () {
+      if (this.animating) {
+        return
+      }
+      if (this.canAutoClose) {
+        this.hide()
+      }
+    },
+    prepare () {
+      this.dot1 = this.$refs['dot-1']
+      this.dot2 = this.$refs['dot-2']
+      this.dot3 = this.$refs['dot-3']
+
+      const steps1 = [{
+        styles: {
+          transform: 'translateX(48px) scale(1)',
+          opacity: 1,
+          perspective: 0
+        },
+        duration: 700, // ms
+        timingFunction: 'cubic-bezier(0.455, 0.03, 0.515, 0.955)'
+      }, {
+        styles: {
+          transform: 'translateX(96px) scale(0.5)',
+          opacity: 1,
+          perspective: 0
+        },
+        duration: 700, // ms
+        timingFunction: 'cubic-bezier(0.455, 0.03, 0.515, 0.955)'
+      }, {
+        styles: {
+          transform: 'translateX(90px) scale(0.3)',
+          opacity: 0,
+          perspective: 0
+        },
+        duration: 200, // ms
+        timingFunction: 'linear'
+      }, {
+        styles: {
+          transform: 'translateX(0px) scale(0.5)',
+          opacity: 1,
+          perspective: 0
+        },
+        duration: 500, // ms
+        timingFunction: 'cubic-bezier(0.455, 0.03, 0.515, 0.955)'
+      }]
+      this.dot1Queue = Queue(this.dot1, steps1)
+
+      const steps2 = [{
+        styles: {
+          transform: 'translateX(48px) scale(0.5)',
+          opacity: 1,
+          perspective: 0
+        },
+        duration: 700, // ms
+        timingFunction: 'cubic-bezier(0.455, 0.03, 0.515, 0.955)'
+      }, {
+        styles: {
+          transform: 'translateX(40px) scale(0.3)',
+          opacity: 0,
+          perspective: 0
+        },
+        duration: 200, // ms
+        timingFunction: 'linear'
+      }, {
+        styles: {
+          transform: 'translateX(-48px) scale(0.5)',
+          opacity: 1,
+          perspective: 0
+        },
+        duration: 500, // ms
+        timingFunction: 'cubic-bezier(0.455, 0.03, 0.515, 0.955)'
+      }, {
+        styles: {
+          transform: 'translateX(0px) scale(1)',
+          opacity: 1,
+          perspective: 0
+        },
+        duration: 700, // ms
+        timingFunction: 'cubic-bezier(0.455, 0.03, 0.515, 0.955)'
+      }]
+      this.dot2Queue = Queue(this.dot2, steps2)
+
+      const steps3 = [{
+        styles: {
+          transform: 'translateX(-6px) scale(0.3)',
+          opacity: 0,
+          perspective: 0
+        },
+        duration: 200, // ms
+        timingFunction: 'linear'
+      }, {
+        styles: {
+          transform: 'translateX(-96px) scale(0.5)',
+          opacity: 1,
+          perspective: 0
+        },
+        duration: 500, // ms
+        timingFunction: 'cubic-bezier(0.455, 0.03, 0.515, 0.955)'
+      }, {
+        styles: {
+          transform: 'translateX(-48px) scale(1)',
+          opacity: 1,
+          perspective: 0
+        },
+        duration: 700, // ms
+        timingFunction: 'cubic-bezier(0.455, 0.03, 0.515, 0.955)'
+      }, {
+        styles: {
+          transform: 'translateX(0px) scale(0.5)',
+          opacity: 1,
+          perspective: 0
+        },
+        duration: 700, // ms
+        timingFunction: 'cubic-bezier(0.455, 0.03, 0.515, 0.955)'
+      }]
+      this.dot3Queue = Queue(this.dot3, steps3)
+    },
+    active () {
+      if (this.show) {
+        return
+      }
+      this.show = true
+      this.loadingStart = new Date().getTime()
+      if (this.isCreator) {
+        this.$nextTick(() => {
+          this.appear(true)
+        })
+      } else {
+        setTimeout(() => {
+          this.appear(true)
+          this.start()
+        }, 20)
+      }
+    },
+    hide () {
+      if (!this.show) {
+        return
+      }
+      this.show = false
+      let timeout = 0
+      if (new Date().getTime() - this.loadingStart <= 100000) {
+        timeout = 100000
+      }
+      setTimeout(() => {
+        this.appear(false)
+        !this.isCreator && this.stop()
+      }, timeout)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .fm-waiting {
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    /*兼容H5异常*/
+    z-index: 999;
+  }
+
+  .waiting-wrapper {
+    align-items: center;
+    height: 240px;
+    border-radius: 18px;
+    background-color: #FFFFFF;
+    padding: 0 36px;
+  }
+
+  .waiting-wrapper-H5 {
+    padding: 0 108px;
+  }
+
+  .waiting--text {
+    font-size: 42px;
+    font-family: sans-serif-medium;
+    font-weight: 500;
+    text-align: center;
+  }
+
+  .waiting-circle {
+    flex-direction: row;
+    justify-content: center;
+    margin: 60px 0;
+  }
+
+  .waiting--dot {
+    z-index: 1;
+    width: 42px;
+    height: 42px;
+    margin: 0 3px;
+    border-radius: 21px;
+    opacity: 1;
+  }
+
+  .waiting--dot-1 {
+    background-color: #4da84f;
+    transform: scale(0.5);
+  }
+
+  .waiting--dot-2 {
+    background-color: #4d79d3;
+    transform: scale(1);
+  }
+
+  .waiting--dot-3 {
+    background-color: #de6a3c;
+    transform: scale(0.5);
+  }
+</style>
