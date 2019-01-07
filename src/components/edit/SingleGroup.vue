df557d19b21008413053665b8837702f8b637229
yangxiaodong0131 <1072702569@qq.com>
2019-01-07 10:05:37 +0800

    修改单条分组

--- src/components/edit/SingleGroup.vue
+++ src/components/edit/SingleGroup.vue
@@ -1,65 +1,42 @@
 <template>
   <div class="container" v-bind:style="panel">
     <mini-bar :title="menu" :rightIcon="['home']" :leftIcon="['left']"></mini-bar>
-    <list class="panel">
+    <list >
       <cell>
-        <div class="row">
-          <div class="col-md-6">
-            <text class="title">主要诊断编码(必填)</text>
-            <input type="text" name="DISEASE_CODE" placeholder="请输入主要诊断编码，如'B25.901'" class="input" value="B25.000y001+J17.1*"/>
-          </div>
-          <div class="col-md-6">
-            <text class="title">年龄(必填)</text>
-            <input type="number" name="AGE" placeholder="请输入年龄" class="input" v-model="wt4.AGE"/>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-6">
-            <text class="title">性别(必填)</text>
-            <input type="text" name="SEX" placeholder="请输入性别" class="input" v-model="wt4.GENDER"/>
-          </div>
-          <div class="col-md-6">
-            <text class="title">住院天数(必填)</text>
-            <input type="number" name="ACCTUAL_DAYS" placeholder="请输入住院天数" class="input" v-model="wt4.ACCTUAL_DAYS"/>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-6">
-            <text class="title">总费用(必填)</text>
-            <input type="text" name="TOTAL_EXPENSE" placeholder="请输入总费用" class="input" v-model="wt4.TOTAL_EXPEMSE"/>
-          </div>
-          <div class="col-md-6">
-            <text class="title">新生儿出生天数</text>
-            <input type="number" name="SF0100" placeholder="请输入新生儿出生天数，缺省填'-1'" class="input" v-model="wt4.SF0100"/>
-          </div>
-        </div>
-        <div class="row">
-          <div class="col-md-6">
-            <text class="title">新生儿入院体重</text>
-            <input type="number" name="SF0102" placeholder="新生儿入院体重，缺省填'-1'" class="input" v-model="wt4.SF0102"/>
-          </div>
-          <div class="col-md-6">
-            <text class="title">呼吸机使用时间</text>
-            <input type="number" name="SF0104" placeholder="呼吸机使用时间，缺省填'-1'" class="input" v-model="wt4.SF0104"/>
-          </div>
+        <div>
+          <am-list :no-border="false">
+            <am-list-input v-model="wt4.B_WT4_V1_ID" title="诊断编码" placeholder="请输入主要诊断编码，如'B25.901'"></am-list-input>
+            <am-list-input v-model="wt4.AGE" title="年龄" placeholder="请输入年龄"></am-list-input>
+            <am-list-input v-model="wt4.GENDER" title="性别" placeholder="请输入性别"></am-list-input>
+            <am-list-input v-model="wt4.ACCTUAL_DAYS" title="住院天数" placeholder="请输入住院天数"></am-list-input>
+            <am-list-input v-model="wt4.TOTAL_EXPEMSE" title="总费用" placeholder="请输入总费用"></am-list-input>
+            <am-list-input v-model="wt4.SF0100" title="新生儿出生天数" placeholder="请输入新生儿出生天数"></am-list-input>
+            <am-list-input v-model="wt4.SF0102" title="新生儿入院体重" placeholder="请输入新生儿入院体重"></am-list-input>
+            <am-list-input v-model="wt4.SF0104" title="呼吸机使用时间" placeholder="请输入呼吸机使用时间"></am-list-input>
+          </am-list>
         </div>
         <div>
-          <text class="title">出院转归(必填)</text>
-          <wxc-grid-select
-            :single="true"
-            :cols="5"
-            :customStyles="customStyles"
-            :list="testData1"
-            @select="params => onSelect('res3', params)">
-          </wxc-grid-select>
+          <am-list :no-border="false">
+            <am-picker
+              title="请选择"
+              :placeholder="'请选择'"
+              :data="testData1"
+              @ok="exchangeOk"
+              @hide="onHide">
+              <am-list-item
+                slot-scope="{ extra, show }"
+                title="出院转归"
+                :extra="extra"
+                @click="show">
+              </am-list-item>
+            </am-picker>
+          </am-list>
         </div>
         <div>
-          <text class="title">其他诊断编码</text>
-          <textarea class="textarea" placeholder="请输入其他诊断编码，中间以逗号分隔，如'B25.901,B25.902" rows='3' name="diags_code"></textarea>
-        </div>
-        <div>
-          <text class="title">手术/操作编码</text>
-          <textarea class="textarea" placeholder="请输入手术/操作编码，中间以逗号分隔，如'B25.901,B25.902" rows='3' name="opers_code"></textarea>
+          <am-list :no-border="false">
+            <am-list-input v-model="wt4.diages_code" title="其他诊断编码" placeholder="请输入其他诊断编码，中间以逗号分隔，如'B25.901,B25.902'"></am-list-input>
+            <am-list-input v-model="wt4.oeprs_code" title="手术/操作编码" placeholder="请输入手术/操作编码，中间以逗号分隔，如'B25.901,B25.902'"></am-list-input>
+          </am-list>
         </div>
         <div class="sub">
           <wxc-button text="提交分组"
@@ -78,11 +55,12 @@
 
 <script>
 import { WxcGridSelect, WxcButton, WxcCell, WxcSimpleFlow } from 'weex-ui'
+import { AmList, AmListInput, AmListItem, AmPicker } from 'weex-amui'
 import { compDrg } from '../../utils/comp'
 import MiniBar from '../common/MiniBar.vue'
 const modal = weex.requireModule('modal')
 export default {
-  components: { WxcGridSelect, WxcButton, MiniBar, WxcCell, WxcSimpleFlow },
+  components: { WxcGridSelect, AmList, AmPicker, AmListItem, AmListInput, WxcButton, MiniBar, WxcCell, WxcSimpleFlow },
   data: () => ({
     themeColor: {
       lineColor: '#bf280b',
@@ -105,15 +83,15 @@ export default {
       backgroundColor: '#ffffff',
       checkedBackgroundColor: '#ffb200'
     },
-    wt4: { B_WT4_V1_ID: -1, DISEASE_CODE: 'B25.000y001+J17.1*', AGE: '12', GENDER: '男', SF0100: -1, SF0102: -1, SF0108: -1, SF0104: -1, ACCTUAL_DAYS: 1, TOTAL_EXPENSE: 1234.5, diages_code: [], oeprs_code: [] },
+    wt4: { B_WT4_V1_ID: -1, DISEASE_CODE: 'B25.000y001+J17.1*', AGE: '12', GENDER: '男', SF0100: -1, SF0102: -1, SF0108: -1, SF0104: -1, ACCTUAL_DAYS: 1, TOTAL_EXPENSE: 1234.5, diages_code: '', oeprs_code: '' },
     testData1: [
-      { title: '缺失' },
-      { title: '医嘱离院' },
-      { title: '医嘱转院', checked: true },
-      { title: '医嘱转入社区' },
-      { title: '非医嘱离院' },
-      { title: '死亡' },
-      { title: '其他' }],
+      { label: '缺失', value: '缺失' },
+      { label: '医嘱离院', value: '医嘱离院' },
+      { label: '医嘱转院', value: '医嘱转院' },
+      { label: '医嘱转入社区', value: '医嘱转入社区' },
+      { label: '非医嘱离院', value: '非医嘱离院' },
+      { label: '死亡', value: '死亡' },
+      { titlabelle: '其他', value: '其他' }],
     testData2: [{ title: '男', checked: true }, { title: '女' }]
   }),
   computed: {
@@ -161,8 +139,14 @@ export default {
     onSelect (res, a) {
       console.log(a)
     },
+    exchangeOk (year, labels) {
+      modal.toast({ message: `已设置出院转归`, duration: 1 })
+    },
     onOverLimit (limit) {
       modal.toast({ message: `最多选择${limit}个`, duration: 0.8 })
+    },
+    onHide (type) {
+      // console.log('hide', type)
     },
     input (e) {
     },
